4d8d782759fc91a804f82c354261295617233a85
Gilles Pilon <gilles.pilon@gillespilon.ca>
2025-03-26 16:55:07 -0500

    Fix errors in coffee_mug_milk

--- tests/test_automation.py
+++ tests/test_automation.py
@@ -35,33 +35,33 @@ def test_water_coffee_tea_milk():
             cups_tea=0,
             mugs_tea=0
         )
-    expected = (370, 220, 150, 0, 0, 150, 20, (0, 2, 5))
+    expected = (370, 220, 150, 0, 0, 225, 20, (0, 2, 5))
     assert result == expected
     result = dd.water_coffee_tea_milk(
             mugs_coffee=1,
             cups_tea=0,
             mugs_tea=1
         )
-    expected = (670, 220, 150, 0, 300, 150, 20, (0, 3, 47))
+    expected = (670, 220, 150, 0, 300, 225, 20, (0, 3, 47))
     assert result == expected
     result = dd.water_coffee_tea_milk(
             mugs_coffee=1,
             cups_tea=1,
             mugs_tea=0
         )
-    expected = (770, 220, 150, 400, 0, 150, 20, (0, 4, 21))
+    expected = (770, 220, 150, 400, 0, 225, 20, (0, 4, 21))
     assert result == expected
     result = dd.water_coffee_tea_milk(
             mugs_coffee=1,
             cups_tea=1,
             mugs_tea=1
         )
-    expected = (1070, 220, 150, 400, 300, 150, 20, (0, 6, 3))
+    expected = (1070, 220, 150, 400, 300, 225, 20, (0, 6, 3))
     assert result == expected
     result = dd.water_coffee_tea_milk(
             mugs_coffee=2,
             cups_tea=0,
             mugs_tea=0
         )
-    expected = (740, 440, 300, 0, 0, 300, 40, (0, 4, 11))
+    expected = (740, 440, 300, 0, 0, 450, 40, (0, 4, 11))
     assert result == expected
